8b9930fc46598344ead8c4d6a5cdf4f6d353b94a
Essy <Essy-Gaming@Essy-HP.(none)>
2016-07-19 12:44:59 -0700

    Covered Security Flaw in GetFromDatastore

--- datastore.go
+++ datastore.go
@@ -1,8 +1,14 @@
 package retrievable
 
 import (
+	"errors"
 	"golang.org/x/net/context"
 	"google.golang.org/appengine/datastore"
+)
+
+var (
+	// ErrGetZero is thrown when this package sees an invalid get key.
+	ErrGetZero = errors.New("Esseh/Retrievable Datastore Error: Zero is not a valid key.")
 )
 
 // Retrievable marks a structure as storable with datastore.
@@ -45,8 +51,25 @@ func PlaceInDatastore(ctx context.Context, key interface{}, source Retrievable) 
 // return the saved struct from datastore.
 //
 // An error may be returned if datastore passes an error.
+//
+// Get int64(0) returns the previously accessed object. This is unintended behavior so
+// it will also return an error.
+// This behavior is insecure, however if the behavior must be used then one level of 
+// misdirection will let it through. ie:
+//   func (a *A) Key(ctx context.Context, key interface{}) *datastore.Key {
+//    return datastore.NewKey(ctx, "tableA", key.(struct{ k int64 }).k, 0, nil)
+//   }
+	
 func GetFromDatastore(ctx context.Context, key interface{}, source Retrievable) error {
 	uk := source.Key(ctx, key)
+	
+	switch key.(type){
+		case int64:
+			if key.(int64) == 0 {
+				return ErrGetZero
+			}
+	}
+	
 	if getErr := datastore.Get(ctx, uk, source); getErr != nil {
 		return getErr
 	}
